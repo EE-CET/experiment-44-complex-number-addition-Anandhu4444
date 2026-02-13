c765369c4faf8118b60c9789a4220449fd8eb2fa
Anandhu4444 <hey4444567889987@gmail.com>
2026-02-13 10:39:59 +0530

    ComplexAddition.java

--- src/ComplexAddition.java
+++ src/ComplexAddition.java
@@ -4,46 +4,44 @@ class Complex {
     double real;
     double imag;
 
-    // Constructor
+    // Constructor to initialize values
     Complex(double real, double imag) {
         this.real = real;
         this.imag = imag;
     }
 
     // Method to add two complex numbers
-    Complex add(Complex c) {
-        double sumReal = this.real + c.real;
-        double sumImag = this.imag + c.imag;
-        return new Complex(sumReal, sumImag);
+    public Complex add(Complex other) {
+        return new Complex(this.real + other.real, this.imag + other.imag);
     }
 
-    // Method to display in a + ib format
-    void display() {
-        if (imag >= 0)
-            System.out.println(real + " + i" + imag);
-        else
-            System.out.println(real + " - i" + (-imag));
+    // Method to display the result in a + ib format
+    public void display() {
+        String sign = (this.imag >= 0) ? "+" : "-";
+        System.out.println(this.real + " " + sign + " i" + Math.abs(this.imag));
     }
 }
 
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Input first complex number
+        // Input for first number
         double r1 = sc.nextDouble();
         double i1 = sc.nextDouble();
+        Complex num1 = new Complex(r1, i1);
 
-        // Input second complex number
+        // Input for second number
         double r2 = sc.nextDouble();
         double i2 = sc.nextDouble();
+        Complex num2 = new Complex(r2, i2);
 
-        Complex c1 = new Complex(r1, i1);
-        Complex c2 = new Complex(r2, i2);
+        // Perform addition
+        Complex result = num1.add(num2);
 
-        Complex result = c1.add(c2);
-
+        // Output result
         result.display();
-
+        
         sc.close();
     }
-
+}
